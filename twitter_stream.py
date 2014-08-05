a0bffcbaa55fb417e39b20f3a4866df9e28178ab
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-08-04 20:20:29 -0700

    fixed up issues with zmq

--- twitter_stream.py
+++ twitter_stream.py
@@ -10,26 +10,42 @@ access_token = '1315274378-RxFpa0iNmDucPaxMCENLaeeky9k4rL9JN0Zjn60'
 access_token_secret = 'fjCiFRyXbIMFSGlykJMEhPxNxpj7YnbFreWoeCJBCLQCG'
 
 tweet_list = ""
+filters = ""
+
 
 # This is the listener, resposible for receiving data
 class StdOutListener(tweepy.StreamListener):
     def on_data(self, data):
+        try:
+            string = socket1.recv_string(zmq.NOBLOCK)
+        except zmq.error.Again:
+            print "Nothing to Receive"
+            string = ""
+        
+        if string:
+            print "*"*100
+            print string
+            filters = [string]
+            stream.disconnect()
+            stream.filter(track=filters)
+            return True
+
         # Twitter returns data in JSON format - we need to decode it first
         decoded = json.loads(data)
 
         # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
         print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
         print ''
-        # f = open('tweets.txt', 'a')
-        # f.write(decoded['text'].encode('ascii', 'ignore'))
-        # f.write('\n+++++++++++++')
-        # f.close()
-        socket.send_string(u"%i %s" % (10, unicode(decoded['text'].encode('ascii', 'ignore'))))
+
+        socket.send_string(u"%s" % (unicode(decoded['text'].encode('ascii', 'ignore'))))
         print "Tweet Sent"
-        # tweet_list = decoded['text'].encode('ascii', 'ignore')
-        # f = open('tweet_pickle', 'wb')
-        # cPickle.dump(tweet_list,f)
-        # f.close()
+
+
+
+
+
+
+        print "Waiting for next tweet..."
         return True
 
     def on_error(self, status):
@@ -44,10 +60,17 @@ if __name__ == '__main__':
     socket = context.socket(zmq.PUB)
     socket.bind("tcp://*:5556")
 
+
+    context1 = zmq.Context()
+    socket1 = context1.socket(zmq.SUB)
+    socket1.connect("tcp://localhost:5559")
+    socket1.setsockopt_string(zmq.SUBSCRIBE, u"")
+
+
     print "Beginning Stream:"
 
     # There are different kinds of streams: public stream, user stream, multi-user streams
     # In this example follow #programming tag
     # For more details refer to https://dev.twitter.com/docs/streaming-apis
     stream = tweepy.Stream(auth, l)
-    stream.filter(track=['macklemore', 'fries'])
+    stream.filter(track=['dude', 'swag', 'bro'])
