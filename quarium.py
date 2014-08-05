1c04bfb43160ef67a1e2329d17b1964bb245f279
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-08-04 20:18:37 -0700

    fixed routing to allow different styles

--- quarium.py
+++ quarium.py
@@ -4,7 +4,7 @@ import sys
 import cPickle
 import json
 import zmq
-
+import time
 from flask import Flask
 from flask import g
 from flask import render_template
@@ -19,28 +19,62 @@ from contextlib import closing
 
 app = Flask(__name__)
 
+def set_subscriber():
+    # Setup tweet subscriber
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:5556")
+    socket.setsockopt_string(zmq.SUBSCRIBE, u"")
+    print "established socket"
+    return socket
 
-def numGen():
-    i = 0
-    while i < 10:
-        i += 1;
-        yield "The number " + str(i)
+
+@app.route('/', methods=['GET'])
+def quarium_style():
+    return render_template('base.html', style='quarium')
 
 
-@app.route('/')
-def show_entries():
+@app.route('/Space', methods=['GET'])
+def space_style():
+    return render_template('base.html', style='space')
 
+
+
+@app.route('/changefilter', methods=['POST'])
+def change_filter():
+    a = socket.close(0)
+    print "closed socket"
+    # try:
+    # except UnboundLocalError:
+    #     pass
+    global socket
+    socket = set_subscriber()
+    # time.sleep(5)
+    # print socket
+    # socket.connect("tcp://localhost:5551")
+    print "got to routing"
+    thefilter = request.form['thefilter']
+    print "sending filter: " + str(thefilter)
+    socket1.send_string(u"%s" % (unicode(thefilter)))
+    print "sent filter"
     # print entries # debugging
-    return render_template('base.html', the_text='No Tweets Yet')
+    return render_template('base.html', style='quarium')
 
 
-@app.route('/test', methods=['GET'])
+
+@app.route('/gettweet', methods=['GET'])
 def contact():
     string = socket.recv_string()
+    try:
+        pass
+    except zmq.error.ZMQError:
+        pass
+        # socket = context.socket(zmq.SUB)
+        # socket.connect("tcp://localhost:5556")
+        # socket.setsockopt_string(zmq.SUBSCRIBE, u"10")
     tweet_list = string.split()
     json_pack = {}
     json_pack['tweet'] = []
-    for word in tweet_list[1:]:
+    for word in tweet_list:
         if 'http' in word:
             json_pack['thelink'] = word
         else:
@@ -48,18 +82,6 @@ def contact():
     json_pack['tweet'] = " ".join(json_pack['tweet'])
     print json_pack['tweet']
     encoded = json.JSONEncoder().encode(json_pack)
-    # name = request.args.get('name', 'Name error')
-    # subject = request.args.get('subject', 'Subject Error')
-    # email = request.args.get('email', 'Email Error')
-    # full_subject = '{} - From: {} @ {}'.format(subject, name, email)
-    # msg = Message(
-    #     full_subject,
-    #     sender=email,
-    #     recipients=['tweet.track@gmail.com']
-    # )
-    # msg.body = request.args.get('message', 'Message error')
-    # mail.send(msg)
-    # return render_template('message_sent.html', name=name)
 
     return encoded
 
@@ -67,7 +89,15 @@ def contact():
 
 if __name__ == '__main__':
     context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.connect("tcp://localhost:5556")
-    socket.setsockopt_string(zmq.SUBSCRIBE, u"10")
-    app.run(debug=True)
+    global socket
+    socket = set_subscriber()
+
+    # Setup filter publisher
+    context1 = zmq.Context()
+    socket1 = context1.socket(zmq.PUB)
+    socket1.bind("tcp://*:5559")
+    # try:
+    # except zmq.error.ZMQError:
+    #     print "Already connected..."
+    app.run()
+    # app.run()
